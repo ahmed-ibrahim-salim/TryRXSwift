932bbad7271db2c6f33cc78a9a0d8d0f3cf1bd4b
Ahmed ibrahim <48734681+ahmed-ibrahim-salim@users.noreply.github.com>
2022-12-21 15:28:45 +0200

    setup project

---
+++ Pods/Target Support Files/Pods-TryRxSwiftNew/Pods-TryRxSwiftNew-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TryRxSwiftNew : NSObject
+@end
+@implementation PodsDummy_Pods_TryRxSwiftNew
+@end
---
+++ TryRxSwiftNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TryRxSwiftNew
+//
+//  Created by magdy khalifa on 21/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
