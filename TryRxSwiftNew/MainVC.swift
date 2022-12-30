ea7e121feed93c3d7b6a129374543d3cca948ea1
Ahmed ibrahim <48734681+ahmed-ibrahim-salim@users.noreply.github.com>
2022-12-30 22:39:16 +0200

    finished chapter 5

--- TryRxSwiftNew/MainVC.swift
+++ TryRxSwiftNew/MainVC.swift
@@ -18,7 +18,87 @@ class MainVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        
+        takeWhileExample()
+    }
+}
+
+extension MainVC{
+    
+    func takeWhileExample(){
+        let disposeBag = DisposeBag()
+        // 1
+          Observable.of(2, 2, 4, 4, 6, 6)
+            // 2
+            .enumerated()
+        // 3
+            .takeWhile { index, integer in
+              // 4
+              integer.isMultiple(of: 2) && index < 3
+            }
+        // 5
+            .map(\.element)
+            // 6
+            .subscribe(onNext: {
+                print($0)
+                
+            })
     }
     
+    func skipUntillExample(){
+        // 1
+          let subject = PublishSubject<String>()
+          let trigger = PublishSubject<String>()
+        // 2
+          subject
+            .skipUntil(trigger)
+            .subscribe(onNext: {
+        print($0) })
+            .disposed(by: disposeBag)
+        // skipped cuz trigger not started yet
+       subject.onNext("A")
+       subject.onNext("B")
+        
+        // trigger started
+        trigger.onNext("X")
+        // now this gets emitted
+        subject.onNext("C")
+    }
+    
+    func skipWhileExample(){
+        // 1
+        Observable.of(2, 2, 3, 4, 4)
+        // 2
+            .skip(while: { $0 == 2 })
+            .subscribe(onNext: {
+                print($0)
+                
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func filterExample(){
+        let strikes = Observable.of(1, 2, 3, 4, 5, 6)
+        
+        strikes
+            .filter { $0.isMultiple(of: 2)}
+            .subscribe(onNext: {
+                print($0)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func elementAt(){
+        let strikes = PublishSubject<String>()
+        // 2
+        strikes
+            .elementAt(2)
+            .subscribe(onNext: { _ in
+                print("You're out!")
+            })
+            .disposed(by: disposeBag)
+        
+        strikes.onNext("X")
+        strikes.onNext("X")
+        strikes.onNext("X")
+    }
 }
